9785bbf9da1ea949c03c45c3f2b01ae0d91030df
jamdz <jamesiucky926@gmail.com>
2020-07-18 08:18:40 -1200

    first inital commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let hamburger = document.querySelector (".hamburger");
+let sideNave = document.querySelector (".side-drop-menu");
+
+hamburger.addEventListener ("click", () => {
+
+    hamburger.classList.toggle ("open");
+    sideNave.classList.toggle ("show");
+});
+
+
+//let hamburger = document.querySelector (".hamburger");
+
+      //let sidenav = document.querySelector (".side-nav");
+
+
+      //hamburger.addEventListener ("click", () => {
+
+          //hamburger.classList.toggle ("open");
+          //sidenav.classList.toggle ("show");
+
+      //});
