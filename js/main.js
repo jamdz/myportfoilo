6366b8e2db5a297bfd77e332d81cc2fb9870cc9f
jamdz <jamesiucky926@gmail.com>
2020-07-20 16:29:05 -1200

    updated the code

--- js/main.js
+++ js/main.js
@@ -8,6 +8,17 @@ hamburger.addEventListener ("click", () => {
 });
 
 
+let newBlog = document.querySelector (".new-blog-4");
+let blogNew = document.querySelector (".new-blog-3");
+let bogNew = document.querySelector (".new-blog-7-11");
+let nogNew = document.querySelector (".new-blog-3-111");
+let newDate = new Date ();
+newBlog.textContent = "By Himel" ;
+blogNew.textContent = "On" + " " + newDate.getMonth() + " " + "june" + " " + newDate.getFullYear() + " " + "Business, Culture" ;
+bogNew.textContent = "By Himel" ;
+nogNew.textContent = "On" + " " + newDate.getMonth() + " " + "june" + " " + newDate.getFullYear() + " " + "Business, Culture" ;
+
+
 //let hamburger = document.querySelector (".hamburger");
 
       //let sidenav = document.querySelector (".side-nav");
